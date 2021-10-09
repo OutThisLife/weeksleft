ec134f107e1fa548873e4feab978c806dda0236d
Talasan Nicholson <talasan.nicholson@gmail.com>
2021-10-08 20:40:03 -0500

    chore: uptick

--- src/shaders/frag.fs
+++ src/shaders/frag.fs
@@ -36,45 +36,14 @@ void main() {
 
   vec2 mo = iMouse;
 
-  vec3 ro = 1. + .07 * cameraPosition;
-  vec3 rd =
-      normalize((cameraWorldMatrix * cameraProjectionMatrixInverse * ndc).xyz);
+  vec3 ro = cameraPosition;
+  vec4 idk = normalize(cameraWorldMatrix * cameraProjectionMatrixInverse * ndc);
+  vec3 rd = idk.xyz / idk.w;
 
   vec3 col;
   float time = iGlobalTime * .1;
 
-  for (int i = 0; i < 4; i++) {
-    float depth = fract(float(i + 13) + time);
-
-    vec3 p = (ro + (float(i + 13 / 3) * rd));
-    vec3 gv = fract(abs(p)) - .5;
-    vec3 id = floor(gv);
-
-    for (int x = -1; x <= 1; x++) {
-      for (int y = -1; y <= 1; y++) {
-        vec3 uv = (gv - vec3(x, y, 0));
-
-        float d = length(uv) - .1;
-        d += d * (.03 / (dot(cross(gv, uv), gv) - dot(uv, uv)));
-
-        float m = .013 / d;
-        m += abs(.004 / d);
-        m *= smoothstep(.8, .1, d);
-
-        vec3 nor = normalize(uv - vec3(x, y, 0.));
-        float dif = clamp(dot(nor, uv), 0., 1.);
-        float amb = clamp(.5 + .5 * nor.y, 0., 1.);
-
-        vec3 lin;
-        lin += 1.5 * dif * vec3(.7);
-        lin += 10. * amb * vec3(.1, 0., .9);
-
-        col += lin * pow(m, 2.);
-      }
-    }
-  }
-
-  // col = mix(col, vec3(1., 0., 0.), s / 64.);
+  col = fract(rd) - .5;
 
   fragColor = vec4(pow(clamp(col, 0., 1.), vec3(1. / 2.2)), 1.);
 }
