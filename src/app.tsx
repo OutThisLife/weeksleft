1239e0d2a2c2fa23ea739abada243b583b92c8c8
Talasan Nicholson <talasan.nicholson@gmail.com>
2021-10-12 09:19:14 -0500

    chore: uptick

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,8 @@ import fragmentShader from './shaders/frag.fs'
 import vertexShader from './shaders/vert.vs'
 
 const Stars: React.FC = () => {
+  const ref = React.useRef<any>()
+
   const positions = React.useMemo(
     () =>
       new Float32Array(
@@ -19,9 +21,58 @@ const Stars: React.FC = () => {
     []
   )
 
+  const colours = React.useMemo(
+    () =>
+      new Float32Array(
+        Array.from([...Array(1e4)]).flatMap(() => [1, 1, 1, 1 + Math.random()])
+      ),
+    []
+  )
+
+  const data = React.useMemo<ShaderMaterialProps>(
+    () => ({
+      fragmentShader: `
+        uniform vec4 color;
+        
+        void main() {
+          vec3 col = vec3(1.);
+          pc_fragColor = vec4(clamp(col, 0., 1.), color.a);
+        }
+      `,
+      side: THREE.DoubleSide,
+      uniforms: {
+        iResolution: new THREE.Uniform(new THREE.Vector3(0, 0, 1))
+      },
+      vertexShader: `
+        uniform vec3 iResolution;
+        
+        void main() {
+          gl_PointSize = 0.1 / iResolution.z;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
+        }
+      `
+    }),
+    []
+  )
+
+  useFrame(({ size: { height, width }, viewport: { dpr } }) => {
+    const $m = ref.current as THREE.Mesh
+
+    if ($m instanceof THREE.Mesh) {
+      const $s = $m.material
+
+      if ($s instanceof THREE.ShaderMaterial) {
+        const w = width * dpr
+        const h = height * dpr
+
+        $s.uniforms.iResolution.value.copy(new THREE.Vector3(w, h, w / h))
+      }
+    }
+  })
+
   return (
     <Billboard>
-      <points>
+      <points {...{ ref }}>
         <sphereBufferGeometry args={[90, 64, 64]}>
           <bufferAttribute
             array={positions}
@@ -29,15 +80,16 @@ const Stars: React.FC = () => {
             count={positions.length / 3}
             itemSize={3}
           />
+
+          <bufferAttribute
+            array={colours}
+            attachObject={['attributes', 'color']}
+            count={colours.length / 4}
+            itemSize={4}
+          />
         </sphereBufferGeometry>
 
-        <pointsMaterial
-          color={0x555555}
-          side={THREE.BackSide}
-          size={0.05}
-          sizeAttenuation
-          transparent
-        />
+        <shaderMaterial {...data} />
       </points>
     </Billboard>
   )
--- src/shaders/frag.fs
+++ src/shaders/frag.fs
@@ -2,10 +2,6 @@
 
 precision highp float;
 
-uniform float iGlobalTime;
-uniform vec3 iResolution;
-
-in vec3 vUv;
 in vec3 vNormal;
 in vec3 vNormel;
 out vec4 fragColor;
@@ -13,12 +9,8 @@ out vec4 fragColor;
 // ----------------------------------------------------------------------
 
 void main() {
-  vec3 col;
-  vec3 uv = vUv * 2. - 1. / iResolution.z;
-
+  vec3 col = vec3(0., 8, 1.);
   float d = pow(1. - dot(vNormal, vNormel), 8.);
-
-  col += vec3(0., 8, 1.);
 
   fragColor = vec4(clamp(col, 0., 1.), smoothstep(0., 4., d));
 }
