cf2df3e71bf61ef2752679dd4a09b4291349070e
Talasan Nicholson <talasan.nicholson@gmail.com>
2021-10-21 16:20:48 -0500

    chore: uptick

--- src/shaders/frag-sphere.fs
+++ src/shaders/frag-sphere.fs
@@ -18,85 +18,83 @@ out vec4 fragColor;
 #define PHI 2.399963229728653
 #define saturate(a) clamp(a, 0., 1.)
 
+// ------------------------------------------------------
+
 vec2 hash(vec2 p) {
   p = vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3)));
-  return -1. + 2. * fract(sin(p) * 43758.5453123);
+  return -1.0 + 2.0 * fract(sin(p) * 43758.5453123);
 }
 
-float noise(in vec2 p) {
-  const float K1 = .366025404;  // (sqrt(3)-1)/2;
-  const float K2 = .211324865;  // (3-sqrt(3))/6;
+float N(float t) { return fract(sin(t * 12345.564) * 7658.76); }
 
+float noise(in vec2 p) {
+  const float K1 = 0.366025404;  // (sqrt(3)-1)/2;
+  const float K2 = 0.211324865;  // (3-sqrt(3))/6;
   vec2 i = floor(p + (p.x + p.y) * K1);
   vec2 a = p - i + (i.x + i.y) * K2;
-  float m = step(a.y, a.x);
-  vec2 o = vec2(m, 1. - m);
-
+  vec2 o = (a.x > a.y)
+               ? vec2(1.0, 0.0)
+               : vec2(0.0, 1.0);  // vec2 of = 0.5 + 0.5*vec2(sign(a.x-a.y),
+                                  // sign(a.y-a.x));
   vec2 b = a - o + K2;
-  vec2 c = a - 1. + 2. * K2;
-  vec3 h = max(.5 - vec3(dot(a, a), dot(b, b), dot(c, c)), 0.);
-  vec3 n = pow(h, vec3(4.)) * vec3(dot(a, hash(i + 0.0)), dot(b, hash(i + o)),
-                                   dot(c, hash(i + 1.0)));
+  vec2 c = a - 1.0 + 2.0 * K2;
+  vec3 h = max(0.5 - vec3(dot(a, a), dot(b, b), dot(c, c)), 0.0);
+  vec3 n =
+      h * h * h * h *
+      vec3(dot(a, hash(i + 0.0)), dot(b, hash(i + o)), dot(c, hash(i + 1.0)));
   return dot(n, vec3(70.0));
 }
 
-float fbm(in vec2 st) {
-  const int o = 4;
-  float value = 0.;
-  float amp = .5;
+mat2 rot(float s, float a) { return mat2(s, -a, a, s); }
 
-  for (int i = 0; i < o; i++) {
-    value += amp * noise(st);
-    amp *= .5;
-    st *= mat2(1.6, 1.2, -1.2, 1.6);
+float pat(vec2 p, float t, float amplitude, float s, float a) {
+  float mask = length(p), amp = amplitude;
+  vec2 n = p;
+
+  for (int i = 0; i < 8; i++) {
+    n *= rot(s, a);
+    t += noise(n) * amp;
+    amp *= amplitude;
   }
 
-  return .5 + .5 * value;
+  return t - mask;
 }
 
-vec2 P(vec3 ro, vec3 rd, float s) { return (ro + s * rd).yx; }
+vec3 mainImage(in vec2 st) {
+  vec3 col;
 
-void mainImage(in vec2 st) {
-  vec3 col = vec3(.3, .5, .9);
-
-  float v = .005;
-  float t = iTime;
-
+  vec3 nor = normalize(vNormal);
   vec3 ro = cameraPosition;
   vec3 rd =
       normalize(cameraWorldMatrix * cameraProjectionMatrixInverse * vPos).xyz;
+  vec2 mo = iMouse / 4.;
 
-  float h = (1. - .1 / sqrt(pow(dot(st, st), 2.)));
+  float t = iTime * .3;
 
   {
-    vec2 p = P(ro, rd, 2.);
-    vec2 turb = .03 * vec2(noise(p * 20.), noise(p * 20.));
-    p += turb;
+    vec2 p = st * 2.5;
+    vec2 pt =
+        vec2(fract(mo.x + p.x - (t / 10.)), fract(mo.y + p.y - (t / 15.)));
 
-    float d =
-        fbm(vec2(p.x - 5. * sin(iTime * v * 2.), p.y - 3. * sin(iTime * v)));
+    float d = pat(p, .5, .4, noise(10. * pt), 4.);
 
-    float d1 = smoothstep(.5, .8, d);
-    col += d1 * vec3(1., 1., 5.);
+    col += vec3(1., .0, .4) * smoothstep(0., .9, d);
   }
 
   {
-    vec2 p = P(ro, rd, .5);
-    vec2 turb = .05 * vec2(noise(p * 1.), noise(p * 2.));
-    p += turb;
+    vec2 p = abs(st);
 
-    float d = fbm(p + 3. * sin(iTime * v));
-    d /= 1.3;
+    float d = pat(p, .1, .8, 1.6, -noise(p + p));
 
-    col += smoothstep(.2, .9, d);
+    col += vec3(0., .8, 1.) * smoothstep(-.1, .7, d);
   }
 
-  fragColor = vec4(saturate(pow(col, vec3(1. / 2.2))), 1.);
+  return col;
 }
 
 void main() {
   vec2 st = (gl_FragCoord.xy - .5 * iResolution.xy) / iResolution.y;
   vec2 uv = ((vUv - .5) * iResolution.xy) / iResolution.y;
 
-  mainImage(uv);
+  fragColor = vec4(saturate(mainImage(uv)), 1.);
 }
