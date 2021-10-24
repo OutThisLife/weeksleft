5c9c5008d56c00d99c62f3bd18f5b23e113ca7e7
Talasan Nicholson <talasan.nicholson@gmail.com>
2021-10-24 09:21:12 -0500

    chore: uptick

--- src/shaders/frag.fs
+++ src/shaders/frag.fs
@@ -15,36 +15,68 @@ out vec4 fragColor;
 
 #define saturate(a) clamp(a, 0., 1.)
 #define S(a, b, c) smoothstep(a, b, c)
+#define ST(a, b) step(a, b)
 
 // ---------------------------------------------------
-
-mat2 R(float a) { return mat2(cos(a), -sin(a), sin(a), cos(a)); }
-mat2 R(float s, float a) { return mat2(s, -a, a, s); }
 
 float triangle(float a) { return abs(fract((a - 1.) / 4.) - .5) * 4. - 1.; }
 
+const vec3 cInner = vec3(.74, .85, .95);
+const vec3 cOuter = vec3(.52, .69, .87);
+const vec3 cBody = vec3(.22, .81, .95);
+
 // ---------------------------------------------------
 
+void sparkle(vec2 p, inout vec3 col) {
+  p = abs(p);
+  float r = length(p), d = .5 / r;
+
+  float body = saturate(S(1.3, 2., d));
+  float outline = saturate(fract(S(.99, 1., body)));
+
+  col = mix(col, cBody, body) + cBody * outline;
+  col = mix(col, pow(cBody, vec3(.3)), S(.7, 0., r / .3));
+}
+
 void main() {
-  vec2 st = (vUv * 2. - 1.) * vResolution.z;
-  vec2 mo = iMouse * vResolution.xy;
+  vec2 st = (vUv * 2. - 1.) / vResolution.z;
+  vec2 mo = iMouse / vResolution.z;
 
   vec3 col;
   float t = iTime * .3;
 
+  // Wings
   {
-    vec2 p = abs(st);
+    float w = .5 + .5 * smoothstep(3., 1., 1. / distance(.5, st.y));
+    vec2 p = abs(st * mat2(w, 0., 0., w));
+    float a = atan(p.y, p.x) * 2.;
+    float r = length(p) * PI;
 
-    float r = length(p);
+    float d = cos(a);
+    d = 1. - smoothstep(d, d + .09, r);
 
-    float d0 = abs(abs(fract(3. * r - t) - .5) - .5);
-    float d1 = abs(abs(fract(4. * d0 + t) - .5) - .5);
-
-    d0 = step(.1, d0);
-    d1 = step(.1, d1);
-
-    col += vec3(.001 / d0, dot(d0, d1), .5 / d1);
+    col += saturate(cOuter * fract(d));
+    col += saturate(cInner * d);
   }
 
-  fragColor = vec4(pow(saturate(col), vec3(1. / 2.2)), 1.);
+  // cBody
+  {
+    vec2 p = st * 2.2;
+
+    {
+      vec2 q = abs(p);
+      float r = length(q);
+      float a = atan(p.x, p.y);
+
+      float tips = S(1., .7, sqrt(a / r));
+      col *= saturate(cOuter + tips);
+
+      float sha = S(-.1, .7, r);
+      col *= saturate((cOuter / 2.) + sha);
+    }
+
+    sparkle(p, col);
+  }
+
+  fragColor = vec4(pow(saturate(col), vec3(1. / 1.)), 1.);
 }
