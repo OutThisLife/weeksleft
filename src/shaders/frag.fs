62bb7bc9b5ee471a7ea7c3cca9420b3c52ce0f3f
Talasan Nicholson <talasan.nicholson@gmail.com>
2021-10-30 14:40:16 -0500

    chore: uptick

--- src/shaders/frag.fs
+++ src/shaders/frag.fs
@@ -25,68 +25,14 @@ out vec4 fragColor;
 
 // ---------------------------------------------------
 
-float rand(vec2 st) {
-  return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
-}
-
-float rand(float s) { return rand(vec2(s, dot(s, s))); }
-
-float noise(in vec2 p) {
-  vec2 i = floor(p);
-  vec2 f = fract(p);
-
-  float a = rand(i);
-  float b = rand(i + vec2(1., 0.));
-  float c = rand(i + vec2(0., 1.));
-  float d = rand(i + vec2(1., 1.));
-
-  vec2 u = f * f * (3. - 2. * f);
-  return mix(a, b, u.x) + (c - a) * u.y * (1. - u.x) + (d - b) * u.x * u.y;
-}
-
-float fbm(vec2 p, float t, float amplitude, float s, float a) {
-  float mask = length(p), amp = amplitude;
-
-  for (int i = 0; i < 8; i++) {
-    p *= R(s, a);
-    t += noise(p) * amp;
-    amp *= amplitude;
-  }
-
-  return t;
-}
-
-float fbm(vec2 p) { return fbm(p, .5, .5, 1.6, 1.2); }
-
-float repeat(vec2 p, float r, float s) {
-  float t = mod(iTime / 2., 1.);
-
-  float th = atan(5. / 8.);
-  mat2 rot = R(cos(th), sin(th));
-
-  vec2 offset = vec2(1, 2) * t * s * rot;
-  vec2 uv = round(rot * (vec2(r) - offset) / s);
-
-  uv = (uv + vec2(-1, 0)) * rot * s + offset;
-
-  return saturate(uv.x);
-}
-
 float tomoe(vec2 p) {
-  float r = length(p), a = atan(p.y, p.x), s = dot(p, p);
+  float r = length(p - .03), a = atan(p.y, p.x), s = dot(p, p);
 
   float d = 1. - saturate(r / .2);
   d *= saturate(sin(a + 12. - 12. * sqrt(s)));
 
   return d;
 }
-
-// ---------------------------------------------------
-
-const vec3 cIris = vec3(1., 0., 0.);
-// const vec3 cIrisGrad = vec3(.9, .6, .2);
-const vec3 cIrisGrad = hue(-2. * dot(cIris, cIris));
-const vec3 cReflection = vec3(1., .9, .8);
 
 // ---------------------------------------------------
 
@@ -102,11 +48,7 @@ void main() {
 
   float r = length(p), a = abs(atan(p.y, p.x)), s = dot(p, p);
 
-  // Tomoe!
-  {
-    vec2 p = st * 1.;
-    col = mix(col, vec3(0.), SM(0., .1, tomoe(p)));
-  }
+  col = mix(col, vec3(0.), SM(0., .1, 2. * tomoe(p)));
 
   fragColor = vec4(pow(saturate(col), vec3(1. / 2.2)), 1.);
 }
