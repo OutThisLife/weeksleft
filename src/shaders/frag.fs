56b70887ad09a341afc188ed16ffcd6800c36ab2
Talasan Nicholson <talasan.nicholson@gmail.com>
2021-11-01 14:24:49 -0500

    chore: uptick

--- src/shaders/frag.fs
+++ src/shaders/frag.fs
@@ -25,8 +25,6 @@ out vec4 fragColor;
 
 // ---------------------------------------------------
 
-float parabola(float x, float k) { return pow(4. * x * (1. - x), k); }
-
 float rand(vec2 st) {
   return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
 }
